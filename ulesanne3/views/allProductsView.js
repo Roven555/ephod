ca583baa1a3b96dd0a5ee5f85cf345f9f1aedc87
Student <student@Administrators-Mac-mini.local>
2025-12-10 08:53:29 +0200

    allproduct

--- ulesanne3/views/allProductsView.js
+++ ulesanne3/views/allProductsView.js
@@ -6,15 +6,23 @@ export const displayAllProductView = (products) => {
     const productsContainer = document.createElement("div");
     productsContainer.classList.add("products-container");
 
-    products.forEach( (product)) => {
+    products.forEach((product) => {
         const productCard = document.createElement("div");
         productCard.classList.add("product");
+
         productCard.innerHTML = `
-        <h3>${product.name}</h3>
-        
-        `
-    }
+            <h3>${product.name}</h3>
+            <p>Kategooria: ${product.category}</p>
+            <p>Hind: $${product.price}</p>
+            <button id="favorites">Lisa lemmikutesse</button>
+        `;
 
+        const cartButton = document.createElement("button");
+        cartButton.textContent = "Lisa ostukorvi";
 
-}
+        productCard.appendChild(cartButton);
+        productsContainer.append(productCard);
+    });
 
+    container.append(productsContainer);
+};
