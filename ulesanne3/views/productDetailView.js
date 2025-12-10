63a436eab189cb453c56d16ec900165192b125fe
Student <student@Administrators-Mac-mini.local>
2025-12-10 10:09:01 +0200

    proddetview

--- ulesanne3/views/productDetailView.js
+++ ulesanne3/views/productDetailView.js
@@ -1,1 +1,16 @@
-export const displayProductDetailView = {product}
+export const displayProductDetailView = (product) => {
+    const container = document.getElementById("main-container");
+    container.innerHTML = "";
+
+    const productCard = document.createElement("div");
+    productCard.classList.add("product");
+
+    productCard.innerHTML = `
+        <h2>${product.name}</h2>
+        <p>Kategooria: ${product.category}</p>
+        <p>Hind: $${product.price}</p>
+        <p>ID: ${product.id}</p>
+    `;
+
+    container.append(productCard);
+};
